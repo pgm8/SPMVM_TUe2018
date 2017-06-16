47b9c315074e6c61a356df064f35a69243b72cd2
Paul Melkert <p.g.melkert@gmail.com>
2017-06-16 14:00:05 +0200

    Adapted Feature normalization script to import and export dataframe objects instead of writing to csv file.

--- source/main/python/data_preprocessing/FeatureNormalizationSP500.py
+++ source/main/python/data_preprocessing/FeatureNormalizationSP500.py
@@ -6,20 +6,21 @@ min max normalization is suitable for both positive and negative values?
 """
 
 # Imports
+
 import os
 from pickle import dump
 from pickle import load
 
 # Load SPX dataframe
-picklename = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))), 'resources/Data/SP500_data.sav')
+picklename = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))),
+                          'resources/Data/SP500_data.sav')
 spx = load(open(picklename, 'rb'))
-
 
 # Rescale feature data (between 0 and 1) with lambda function
 spx.iloc[:, 1:-1] = spx.iloc[:, 1:-1].apply(lambda x: (x - x.min()) / (x.max() - x.min()))
 
-
-# Save normalized feature data with pickle
-picklename = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))), 'resources/Data/SP500_data_Norm.sav')
+# Save union of normalized feature matrix and response vector with pickle
+picklename = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))),
+                          'resources/Data/SP500_data_Norm.sav')
 dump(spx, open(picklename, 'wb'))
 
