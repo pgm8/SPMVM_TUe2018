ea5e9e413393dcfeb8e53752fd91e5fea23bd963
Paul Melkert <p.g.melkert@gmail.com>
2017-07-05 17:15:14 +0200

    #20: Replaced pickle loading with call to pickle loader function of module manager instance.

--- source/main/python/data_preprocessing/PreProcessingMain.py
+++ source/main/python/data_preprocessing/PreProcessingMain.py
@@ -2,35 +2,32 @@ from pandas_datareader import data as dt
 from ModuleManager import ModuleManager
 from TechnicalAnalyzer import TechnicalAnalyzer
 from FeatureNormalizer import FeatureNormalizer
-import os
-from pickle import load
-import pandas as pd
 
 
 def data_retrieval():
-    """Method to pull financial data from yahoo.finance API does not seem to work anymore."""
-    tickers = ['SPY'] # ETF SPY as proxy for S&P 500
+    """Method to pull financial data from yahoo.finance. Note: API does not seem to work anymore
+    due to changes of webpage structure at server's end."""
+    tickers = ['SPY']  # ETF SPY as proxy for S&P 500
     data_source = 'google'
     start = '1992-01-01'
     end = '2016-12-31'
     data_panel = dt.DataReader(tickers, data_source=data_source, start=start, end=end)
-    print(type(data_panel))  # Weird ass data panel
+    print(type(data_panel))  # Weird ass data structure: panel
 
 
 def main():
     mm = ModuleManager()
     ta = TechnicalAnalyzer()
     ft = FeatureNormalizer()
-    # Transform csv files to pickled objects
+
     filenames = ['GSPC.csv']
     for filename in filenames:
+        # Transform csv files to pickled objects
         mm.transform_csv_to_pickle(filename)
         # Load pickled files
-        path = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))),
-                            ('resources/Data/%s' % filename[:-4] + '_csv.pkl'))
-        data = load(open(path, 'rb'))
-        # Feature Engineering
-        # Create pickled dataframes obtained form technical analysis:
+        data = mm.load_data(filename[:-4] + '_csv.pkl')
+        # Feature Engineering and Normalization:
+        # Create pickled dataframes obtained form technical analysis
         # 1a) base: price and volume data + return + response
         data_ta_base = ta.ta_base(data)
         mm.save_data(filename[:-4] + '_base.pkl', data_ta_base)
