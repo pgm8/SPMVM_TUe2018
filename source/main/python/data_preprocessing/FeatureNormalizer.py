05a963620319ab6bf9998c63fbf4767263cb6459
Paul Melkert <p.g.melkert@gmail.com>
2017-07-05 16:58:14 +0200

    #20: Refactoring feature normalizer class.

--- source/main/python/data_preprocessing/FeatureNormalizer.py
+++ source/main/python/data_preprocessing/FeatureNormalizer.py
@@ -6,26 +6,16 @@ class FeatureNormalizer(object):
     """Feature normalization class. This class has the responsibility to normalize the feature space.
     It does so by transforming all feature values to values in the interval [0, 1]."""
 
-    def __init__(self, data):
-        """Initializer FeatureNormalizer object.
-        :param data: set representing the entire data (feature matrix and response vector)."""
-        self.data = data
-        self.normalizeFeatureMatrix(data)
+    def __init__(self):
+        """Initializer FeatureNormalizer object."""
 
-    def normalizeFeatureMatrix(self, data):
+    def normalize_feature_matrix(self, data):
         """Method for data normalization.
         :param data: set representing the entire data (feature matrix and response vector).
         :return: normalized data set."""
         data.iloc[:, 1:-1] = data.iloc[:, 1:-1].apply(lambda x: (x - x.min()) / (x.max() - x.min()))
         return data
 
-    def saveNormalizedData(self, data, filename):
-        """Method for writing normalized data to resource directory.
-        :param data: normalized data set
-        :param filename: name of object containing normalized data set."""
-        path = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))), ('resources/Data/%s'
-                                                                                          % filename))
-        dump(data, open(path, 'wb'))
 
 
 
